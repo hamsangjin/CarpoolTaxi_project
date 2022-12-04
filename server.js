e8a2fa6d6dec3cac0580731151b6e4602998376e
hamsangjin <rhkstlash@naver.com>
2022-12-04 19:46:37 +0900

    택시 추가

--- server.js
+++ server.js
@@ -156,6 +156,95 @@ app.delete('/api/carpoolboard/:boarduserId', (req, res)=>{
 
 // 택시 (driver, car 제외하고 구현하면 됨, 그리고 신청할 때 무조건 동승자임)
 
+// 전체 게시물 조회
+app.get('/api/taxiboard',(req,res)=>{
+    connection.query('SELECT * FROM taxiBoard', function (error, rows, fields) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(rows);
+        console.log(rows);
+      })
+})
+
+// 자신의 게시물 조회
+app.get('/api/taxiboard/:writer', (req,res)=>{
+    connection.query('SELECT * FROM taxiBoard WHERE writer = ?', req.params.writer, function (error, rows, fields) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(rows);
+      })
+})
+
+// 해당 게시물 동승자 조회
+app.get('/api/taxipassenger/:boardId', (req,res)=>{
+    connection.query('SELECT * FROM taxiPassenger WHERE boardId=?',req.params.boardId, function (error, rows, fields) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(rows);
+      })
+})
+
+// 게시물 추가
+app.post('/api/taxiboard', (req,res)=>{
+    let sql = 'INSERT INTO taxiBoard VALUES (null,?,?,?,?,?,?,?,?,?,?,?,?)';
+    let title = req.body.title;
+    let writer = req.body.writer;
+
+    let startProvince = req.body.startProvince;
+    let startCity = req.body.startCity;
+    let startDetail = req.body.startDetail;
+
+    let arrivalProvince = req.body.arrivalProvince;
+    let arrivalCity = req.body.arrivalCity;
+    let arrivalDetail = req.body.arrivalDetail;
+
+    let date = req.body.date;
+    let time = req.body.time;
+
+    let maxPassenger = req.body.maxPassenger;
+    
+    let content = req.body.content;
+
+    let params = [title,writer,startProvince,startCity,startDetail,arrivalProvince,arrivalCity,arrivalDetail,date,time,maxPassenger,content]
+    console.log(req.body)
+    connection.query(sql, params, (err,rows,fields)=>{
+        res.header("Access-Control-Allow-Origin", "*")
+        res.send(rows);
+    })
+    
+})
+
+// 게시글 동승자 추가
+app.post('/api/taxipassenger', (req,res)=>{
+    let sql = 'INSERT INTO taxiPassenger VALUES (?,?,?)'
+    let boarduserId = req.body.boarduserId;
+    let boardId = req.body.boardId;
+    let userId = req.body.userId;
+    
+    let params = [boarduserId, boardId, userId]
+    connection.query(sql, params, function(error, rows, field){
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(rows)
+    })
+})
+
+// 게시글 삭제
+app.delete('/api/taxiboard/:boardId', (req, res)=>{
+    console.log(req.params.boardId)
+    connection.query('DELETE FROM taxiBoard WHERE boardId = ?', req.params.boardId, function (error, rows, fields) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(rows);
+        console.log(rows);
+      })
+})
+
+// 게시글 동승자 삭제
+app.delete('/api/taxiboard/:boarduserId', (req, res)=>{
+    console.log(req.params.boarduserId)
+    connection.query('DELETE FROM taxiPassenger WHERE boarduserId = ?', req.params.boarduserId, function (error, rows, fields) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(rows);
+        console.log(rows)
+      })
+})
+
 app.listen(port, (req,res)=>{
     console.log("서버 작동")
 })
